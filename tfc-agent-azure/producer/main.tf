5f16b9f7bc507e60328d7dce1cccbdcfbf2a6260
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 16:09:44 -0700

    ip

--- tfc-agent-azure/producer/main.tf
+++ tfc-agent-azure/producer/main.tf
@@ -52,14 +52,11 @@ resource "azurerm_container_group" "tfc-agent" {
   name                = "tfc-agent"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  ip_address_type     = "Private"
-  #subnet_ids          = data.azurerm_subnet.internal.id
   os_type             = "Linux"
   restart_policy      = "Always"
+  ip_address_type     = "Private"
+  subnet_ids          = [ data.azurerm_subnet.internal.id ]
 
-  network_profile = {
-    id = data.azurerm_subnet.internal.id
-  }
 
   container {
     name   = "tfc-agent"
