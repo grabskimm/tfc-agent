ffe20692f14d3b4ba2658001547a1ec4cb84b71b
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 14:22:55 -0700

    Update main.tf

--- tfc-agent-azure/consumer/main.tf
+++ tfc-agent-azure/consumer/main.tf
@@ -7,25 +7,6 @@ provider "azurerm" {
   use_msi         = true
 }
 
-data "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-w3-net-rg"
-  #location = var.location
-}
-
-data "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-w3-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
-}
-
-data "azurerm_subnet" "internal" {
-  name                 = "${var.prefix}-snet-tools"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = data.azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.16.0/20"]
-}
-
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-tfc-agent-nic"
   resource_group_name = data.azurerm_resource_group.main.name
