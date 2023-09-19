e3c7d654f4b2bc57d38cf002631cc4c514cdf479
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 15:13:18 -0700

    Update variables.tf

--- tfc-agent-azure/producer/variables.tf
+++ tfc-agent-azure/producer/variables.tf
@@ -39,10 +39,6 @@ variable "notification_token" {
   default     = "SuperSecret!!"
 }
 
-variable "resource_group_name" {
-  description = "The name of an existing resource group where the containerized agents will be deployed."
-}
-
 variable "tfc_agent_token" {
   description = "Terraform Cloud agent token. (mark as sensitive) (TFC Organization Settings >> Agents)"
 }
