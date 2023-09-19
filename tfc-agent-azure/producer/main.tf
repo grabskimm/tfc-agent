649f1efba395ddd4db28a78db2af0651e9dc43fd
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 15:28:35 -0700

    Update main.tf

--- tfc-agent-azure/producer/main.tf
+++ tfc-agent-azure/producer/main.tf
@@ -188,8 +188,8 @@ resource "azurerm_function_app" "function_app" {
 
 resource "azurerm_log_analytics_workspace" "workspace" {
   name                = "tfc-agent-log-workspace"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
