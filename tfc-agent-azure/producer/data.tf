bd7c401c875b464589773e765abdf67a3df7cba5
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 16:29:38 -0700

    update subnet

--- tfc-agent-azure/producer/data.tf
+++ tfc-agent-azure/producer/data.tf
@@ -1,18 +1,14 @@
 data "azurerm_resource_group" "main" {
   name     = "${var.prefix}-w3-net-rg"
-  #location = var.location
 }
 
 data "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-w3-vnet"
-  #address_space       = ["10.0.0.0/16"]
-  #location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
 data "azurerm_subnet" "internal" {
-  name                 = "${var.prefix}-w3-snet-tools"
+  name                 = "${var.prefix}-w3-snet-tfc-agent"
   resource_group_name  = data.azurerm_resource_group.main.name
   virtual_network_name = data.azurerm_virtual_network.main.name
-  #address_prefixes     = ["10.0.16.0/20"]
 }
