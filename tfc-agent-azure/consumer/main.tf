279250d0fe32edbc1b35da422dfd136562f0a318
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 13:30:21 -0700

    Update main.tf

--- tfc-agent-azure/consumer/main.tf
+++ tfc-agent-azure/consumer/main.tf
@@ -15,14 +15,14 @@ data "azurerm_resource_group" "main" {
 data "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-w3-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
 }
 
 data "azurerm_subnet" "internal" {
   name                 = "${var.prefix}-snet-tools"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = data.azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.16.0/20"]
 }
 
