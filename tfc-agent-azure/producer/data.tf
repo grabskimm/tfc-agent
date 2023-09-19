2fe8f66d2cd8f825cfd932a5fae13e3d210293cd
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 16:46:41 -0700

    w3

--- tfc-agent-azure/producer/data.tf
+++ tfc-agent-azure/producer/data.tf
@@ -3,12 +3,12 @@ data "azurerm_resource_group" "main" {
 }
 
 data "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-w3-vnet"
+  name                = "${var.prefix}-vnet"
   resource_group_name = data.azurerm_resource_group.main.name
 }
 
 data "azurerm_subnet" "internal" {
-  name                 = "${var.prefix}-w3-snet-tfc-agent"
+  name                 = "${var.prefix}-snet-tfc-agent"
   resource_group_name  = data.azurerm_resource_group.main.name
   virtual_network_name = data.azurerm_virtual_network.main.name
 }
