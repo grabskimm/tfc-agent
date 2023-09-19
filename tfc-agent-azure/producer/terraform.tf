898b5fb6909afd416fb06a2650ae53943ca14182
Mendel <60679806+grabskimm@users.noreply.github.com>
2023-09-19 16:23:33 -0700

    update

--- tfc-agent-azure/producer/terraform.tf
+++ tfc-agent-azure/producer/terraform.tf
@@ -1,11 +1,8 @@
 terraform {
   required_version = ">= 0.10.0"
-}
-
-required_providers {
-
+  required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
       version = "3.47.0"
     }
+  }
 }
